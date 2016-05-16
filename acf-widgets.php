72f4c9230890b94df101f694c67d1916d2544e74
Daron <daronspence@gmail.com>
2016-05-15 22:47:49 -0500

    fix typo in version number

--- acf-widgets.php
+++ acf-widgets.php
@@ -15,7 +15,7 @@ if ( !defined('ABSPATH') ){
 	die();
 }
 
-define( 'ACFW_VERSION', '1.8 );
+define( 'ACFW_VERSION', '1.8' );
 define( 'ACFW_STORE_URL', 'http://acfwidgets.com' );
 define( 'ACFW_ITEM_NAME', 'ACF Widgets' );
 define( 'ACFW_FILE' , __FILE__ );
